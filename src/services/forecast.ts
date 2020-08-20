ff2a369a015252af7c1595fbfb36199ba778b489
Diego Lopes <diego-chez@hotmail.com>
2020-08-20 09:06:08 -0300

    feat: clusterize forecasts by hour

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -17,12 +17,17 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+  time: string;
+  forecast: Array<BeachForecast>;
+}
+
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Array<Beach>
-  ): Promise<Array<BeachForecast>> {
+  ): Promise<Array<TimeForecast>> {
     const pointsWithCorrectSources: Array<BeachForecast> = [];
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -38,6 +43,24 @@ export class Forecast {
       }));
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(
+    forecast: Array<BeachForecast>
+  ): Array<TimeForecast> {
+    const foreCastByTime: Array<TimeForecast> = [];
+    for (const point of forecast) {
+      const timePoint = foreCastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        foreCastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+    return foreCastByTime;
   }
 }
