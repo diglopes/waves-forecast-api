571f7bbda82b62c76c08aa3b9cd2f2d961a2825c
Diego Lopes <diego-chez@hotmail.com>
2020-08-20 20:47:08 -0300

    test: ensure that an empty array is returned

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -25,4 +25,10 @@ describe('Forecast Service', () => {
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
     expect(beachesWithRating).toEqual(forecastExpectedResponse);
   });
+
+  it('should return an empty list when the beaches array is empty', async () => {
+    const forecast = new Forecast();
+    const response = await forecast.processForecastForBeaches([]);
+    expect(response).toEqual([]);
+  });
 });
