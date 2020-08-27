c0d5f9a99f7993a1e09e1d04cd2bdfbb4421e8ab
Diego Lopes <diego-chez@hotmail.com>
2020-08-27 07:47:49 -0300

    feat: add auth middleware

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import config from 'config';
+import { UsersCotroller } from '@src/controllers/users';
+
+export interface DecodedUser extends Omit<UsersCotroller, '_id'> {
+  id: string;
+}
 
 export default class AuthService {
   public static async hashPassword(
@@ -17,9 +22,13 @@ export default class AuthService {
     return await bcrypt.compare(password, hashedPassword);
   }
 
-  public static gerenateToken(payload: Record<string, unknown>): string {
+  public static gerenateToken(payload: object): string {
     return jwt.sign(payload, config.get('App.auth.secret'), {
       expiresIn: config.get('App.auth.tokenExpiresIn'),
     });
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, config.get('App.auth.secret')) as DecodedUser;
   }
 }
